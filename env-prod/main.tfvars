e64b7c60990efb5772a306176b4846c61e888fd2
saketh <uday.kundoor123@gmail.com>
2024-04-14 21:14:48 +0530

    t3-small

--- env-prod/main.tfvars
+++ env-prod/main.tfvars
@@ -1,4 +1,4 @@
 env = "prod"
-instance_type = "t2.micro"
+instance_type = "t3.small"
 ssh_user = "ec2-user"
 ssh_pass = "DevOps321"
--- modules/app/main.tf
+++ modules/app/main.tf
@@ -13,7 +13,7 @@ resource "null_resource" "ansible" {
       connection {
         type = "ssh"
         user = var.ssh_user
-        password = "DevOps321"
+        password = var.ssh_pass
         host = aws_instance.instance.public_ip
       }
           inline = [
