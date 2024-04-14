aacb3042c0d640e1293ad1154f00a5939bed5bdc
saketh <uday.kundoor123@gmail.com>
2024-04-14 15:08:00 +0530

    ssh_pass=var.ssh_pass

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ module "frontend" {
  instance_type = var.instance_type
  component = "frontend"
  ssh_user = var.ssh_user
- ssh_pass = var.ssh_user
+ ssh_pass = var.ssh_pass
  zone_id = var.zone_id
 }
