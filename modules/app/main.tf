b49bec2faaa5fc3af353bba8839a674a82ecd73e
saketh <uday.kundoor123@gmail.com>
2024-04-19 17:04:41 +0530

    modified ansible pull command

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -28,8 +28,8 @@ resource "null_resource" "ansible" {
       }
           inline = [
       "sudo pip3.11 install ansible",
-      "ansible-pull -i localhost, -U https://github.com/raghudevopsb78/expense-ansible get-secrets.yml -e env=${var.env} -e role_name=${var.component}  -e vault_token=${var.vault_token}",
-      "ansible-pull -i localhost, -U https://github.com/saketh-devops-i1/learn-ansible.git expense.yml -e env=${var.env} -e role_name=${var.component}"
+      "ansible-pull -i localhost, -U https://github.com/saketh-devops-i1/learn-ansible.git get-secrets.yml -e env=${var.env} -e role_name=${var.component}  -e vault_token=${var.vault_token}",
+      "ansible-pull -i localhost, -U https://github.com/saketh-devops-i1/learn-ansible.git expense.yml -e env=${var.env} -e role_name=${var.component} -e @common.json -e @${var.component}.json",
           ]
      }
     }
