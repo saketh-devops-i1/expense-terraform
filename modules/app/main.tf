0915e95ada7509b0cf37e69a633728e674634f36
saketh <uday.kundoor123@gmail.com>
2024-04-19 18:38:24 +0530

    vault

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -29,7 +29,7 @@ resource "null_resource" "ansible" {
           inline = [
       "sudo pip3.11 install ansible hvac",
       "ansible-pull -i localhost, -U https://github.com/saketh-devops-i1/learn-ansible.git get-secrets.yml -e env=${var.env} -e role_name=${var.component}  -e vault_token=${var.vault_token}",
-      "ansible-pull -i localhost, -U https://github.com/saketh-devops-i1/learn-ansible.git expense.yml -e env=${var.env} -e role_name=${var.component} -e @secrets.json -e @app.json",
+      "ansible-pull -i localhost, -U https://github.com/saketh-devops-i1/learn-ansible.git expense.yml -e env=${var.env} -e role_name=${var.component} -e @~/secrets.json -e @~/app.json",
           ]
      }
     }
