0ca3a59ed9d99da74819f6d24c45bdcdea431f31
saketh <uday.kundoor123@gmail.com>
2024-04-20 23:01:38 +0530

    corrected variables

--- modules/vpc/variable.tf
+++ modules/vpc/variable.tf
@@ -1,10 +1,10 @@
 variable "env" {}
 variable "vpc_cidr_block" {}
-variable frontend_subnets {}
-variable backend_subnets {}
-variable db_subnets {}
-variable public_subnets {}
-variable availability_zones {}
+variable "frontend_subnets" {}
+variable "backend_subnets" {}
+variable "db_subnets" {}
+variable "public_subnets" {}
+variable "availability_zones" {}
 variable "default_vpc_id" {}
 variable "default_vpc_cidr" {}
 variable "default_route_table_id" {}
