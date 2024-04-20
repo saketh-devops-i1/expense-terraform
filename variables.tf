55072346cd0f40448778abc01d6d62a9244ce7d8
saketh <uday.kundoor123@gmail.com>
2024-04-20 23:05:50 +0530

    error fix

--- variables.tf
+++ variables.tf
@@ -3,7 +3,6 @@ variable "instance_type" {}
 variable "zone_id" {}
 variable "vault_token" {}
 variable "vpc_cidr_block" {}
-variable "subnet_cidr_block" {}
 variable "default_vpc_id" {}
 variable "default_vpc_cidr" {}
 variable "default_route_table_id" {}
