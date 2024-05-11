870634a79f8e25e97fb2491c14627a43a7b25b5d
saketh <uday.kundoor123@gmail.com>
2024-05-11 16:51:49 +0530

    health check in target group of load balancer

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -99,6 +99,15 @@ resource "aws_lb_target_group" "main" {
   port                 = var.app_port
   protocol             = "HTTP"
   vpc_id               = var.vpc_id
+
+  health_check {
+    healthy_threshold   = 2
+    interval            = 5
+    path                = "/health"
+    port                = var.app_port
+    timeout             = 2
+    unhealthy_threshold = 2
+  }
 }
 
 resource "aws_lb_target_group_attachment" "main" {
